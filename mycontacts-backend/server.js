8308f4aa8e79b3a2de9ce8c409e74b7a4060d087
Enrique Austin <34680513+EnriqueAustin@users.noreply.github.com>
2023-03-29 03:20:06 +0200

    dbConnection.js created and imported into server.js

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,8 +1,10 @@
 // IMPORTS
 const express = require('express');
+const connectDb = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 
+connectDb();
 const app = express();
 
 const port = process.env.PORT || 5000;
